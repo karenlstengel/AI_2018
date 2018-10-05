bf7120399b62ceda7b3ac0ab77d5281e71b89770
karenlstengel <#Justdance1>
2018-10-05 14:39:39 -0600

    pseudocode for BFS pt2

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -4,17 +4,32 @@
 
  */
 
+import java.util.LinkedList;
+
 public class BreadthFirstSearch implements IAlgorithm{
     @Override
     public void search(Maze maze){
-        Node start = maze.getStart();
+        Node node = maze.getStart();
         boolean found = false;
+        LinkedList<Node> frontier = new LinkedList<Node>();
+        frontier.addFirst(node);
 
-        //while node != goal && node has children
-            // for each child of node that hasn't been visited
+        //while node has children
+        while(!node.getConnections().isEmpty()) {
+            // for each child of node
+                //if the child has been visited:
+                    // if child cost < cost + 1
+                    //don't change parent
+                //else cost = cost(node) + 1
+                    //set parent = node
+
                 // check if they are the goal node.
-                // cost = cost(node) + 1
-                    //if they are goal; set parent = node and set found = true
+                    //if they are goal; set found = true
+                        // dont add child to queue
                     //else add child to queue and mark as visited.
+            // remove node from queue
+            // set fronteir.getfirst() as node
+        }
+
     }
 }

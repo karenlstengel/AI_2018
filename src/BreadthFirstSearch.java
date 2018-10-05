1a27c1a7eb6475e6f1f993bebaf4e957302e37fb
karenlstengel <#Justdance1>
2018-10-05 12:54:26 -0600

    pseudocode for BFS

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -7,6 +7,14 @@
 public class BreadthFirstSearch implements IAlgorithm{
     @Override
     public void search(Maze maze){
+        Node start = maze.getStart();
+        boolean found = false;
 
+        //while node != goal && node has children
+            // for each child of node that hasn't been visited
+                // check if they are the goal node.
+                // cost = cost(node) + 1
+                    //if they are goal; set parent = node and set found = true
+                    //else add child to queue and mark as visited.
     }
 }

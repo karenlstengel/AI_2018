b2ffea41f6a299080d110880ca4c8e4745115125
invigoro <35319035+invigoro@users.noreply.github.com>
2018-10-05 15:37:37 -0600

    Add files via upload

---
+++ AI_Project_V2/src/src/Maze.java
@@ -0,0 +1,277 @@
+/*
+    @author Karen Stengel
+    Maze class to create and maintain an array of Nodes representing the maze
+ */
+
+
+import java.io.*;
+
+public class Maze{
+    private Node[][] maze;
+    private Node start;
+    private Node goal;
+    public IAlgorithm search;
+
+    //constructor.
+    // read in file and save to the maze array. if the char == P, create a new node for start; if char ==*, create a goal node
+    public Maze(String file){
+        File mazeFile = new File(file);
+        maze = readMazeFile(mazeFile);
+    }
+
+    //readMazeFile
+    //read in the file using a scanner class instance.
+    public Node[][] readMazeFile(File file){
+        Node[][] mArray = null;
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(file));
+
+            int rows = 0;
+            int col = 0;
+            String line;
+            while((line = in.readLine()) != null){
+                rows++;
+                col = line.length();
+            }
+
+            BufferedReader inMaze = new BufferedReader(new FileReader(file));
+
+            mArray = new Node[rows][col];
+
+            for(int i = 0; i < rows; i++){
+                String inLine = inMaze.readLine();
+                for(int j = 0; j < inLine.length(); j++) {
+                    char c = inLine.charAt(j);
+                    mArray[i][j] = new Node(i,j,c);
+                }
+            }
+
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mArray;
+    }
+
+    //build maze.
+    //go thru each node in the array ignoring teh boundaries since they should not be added as possible moves.
+    // for each non-wall node,add all non-wall nodes to connections unless a given node is already in connections.
+    // also add the current node to each of its connections' connections unless it is already there
+    // moves to the children lists. continue until every non % has been added  (should have a skeleton form of the maze
+    public void buildGraph(){
+        //leave parent as null until search/solution is done
+        //want to ignore the boundaries
+        for(int i = 1; i < maze.length - 1; i++){
+            for(int j = 1; j < maze[0].length - 1; j++){
+                //System.out.print(maze[i][j].getSymbol());
+                //if getSymbol == ' '
+                if(maze[i][j].getSymbol() == ' '){
+                    //set connections
+                    if(maze[i - 1][j].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i-1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i-1][j]);
+                        }
+                        if(!maze[i-1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i-1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i + 1][j].getSymbol() != '%'){
+                        //set connections
+
+                        if(!maze[i][j].getConnections().contains(maze[i+1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i+1][j]);
+                        }
+                        if(!maze[i+1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i+1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j - 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j - 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j - 1]);
+                        }
+                        if(!maze[i][j - 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j - 1].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j + 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j + 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j + 1]);
+                        }
+                        if(!maze[i][j + 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j + 1].addChild(maze[i][j]);
+                        }
+                    }
+                }
+                //elseif getSymbol == '*'
+                else if( maze[i][j].getSymbol() == '*'){
+                    // set connections and set as goal node
+                    goal = maze[i][j];
+                    if(maze[i - 1][j].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i-1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i-1][j]);
+                        }
+                        if(!maze[i-1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i-1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i + 1][j].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i+1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i+1][j]);
+                        }
+                        if(!maze[i+1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i+1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j - 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j - 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j - 1]);
+                        }
+                        if(!maze[i][j - 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j - 1].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j + 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j + 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j + 1]);
+                        }
+                        if(!maze[i][j + 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j + 1].addChild(maze[i][j]);
+                        }
+                    }
+                }
+                //elseif getSymbol == 'P'
+                else if(maze[i][j].getSymbol() == 'P'){
+                    //set connections and set as start node
+                    start = maze[i][j];
+                    if(maze[i - 1][j].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i-1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i-1][j]);
+                        }
+                        if(!maze[i-1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i-1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i + 1][j].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i+1][j])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i+1][j]);
+                        }
+                        if(!maze[i+1][j].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i+1][j].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j - 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j - 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j - 1]);
+                        }
+                        if(!maze[i][j - 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j - 1].addChild(maze[i][j]);
+                        }
+                    }
+                    if(maze[i][j + 1].getSymbol() != '%'){
+                        //set connections
+                        if(!maze[i][j].getConnections().contains(maze[i][j + 1])){
+                            //add next node to i,j's children list
+                            maze[i][j].addChild(maze[i][j + 1]);
+                        }
+                        if(!maze[i][j + 1].getConnections().contains(maze[i][j])){
+                            //add next node to i,j's children list
+                            maze[i][j + 1].addChild(maze[i][j]);
+                        }
+                    }
+                }
+                //else (getSymbol) == '%'
+                else{
+                    // do nothing; remove. just for visualization
+                    //System.out.println("You are a wall.");
+                }
+            }
+        }
+    }
+
+    //maze solution
+    // backtrack the final solution path and change those coordinate in the maze to a '.'
+    public void setSolution(){
+        Node n = goal;
+        while(n != start){
+            Node next = n.getParent();
+            maze[next.getX()][next.getY()].setSymbol('.');
+            n = next;
+        }
+    }
+
+    //print maze
+    // basic array print
+    public void printMaze(){
+
+        for(int i = 0; i <maze.length; i++){
+            for(int j = 0; j < maze[0].length; j++){
+                System.out.print(maze[i][j].getSymbol());
+            }
+            System.out.println();
+        }
+    }
+
+    public void resetMaze()
+    {
+        for(int i = 0; i < maze.length; i++)
+        {
+            for(int j = 0; j < maze[0].length; j++)
+            {
+                maze[i][j].setPointer(null);
+                maze[i][j].setUnvisited();
+            }
+        }
+        //change everything back to unvisited and delete CopyNode pointers
+    }
+    public Node[][] getMaze()
+    {
+        return maze;
+    }
+
+    //set search type
+    // change search var to whichever search is desired
+    public void setSearch(IAlgorithm s){
+        search = s;
+    }
+    public Node getStart(){
+        return start;
+    }
+    public Node getGoal(){
+        return goal;
+    }
+
+}
---
+++ AI_Project_V2/src/src/Node.java
@@ -0,0 +1,94 @@
+/*
+    @author Karen Stengel, Jared Thompson
+    Node class for each spot of the maze and storing a nodes local connections
+ */
+
+import java.util.ArrayList;
+
+public class Node{
+    private int x;
+    private int y;
+    private ArrayList<Node> connections;
+    private Boolean visited;
+    private Node parent;
+    private CopyNode pointerNode;
+    private char symbol;
+    private int cost;
+
+    //constructor
+    public Node(int x, int y, char symbol){
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+        visited = false;
+        cost = 0;
+        connections = new ArrayList<Node>();
+    }
+
+    //set methods
+
+    public void setVisited(){
+        visited = true;
+    }
+
+    public void addChild(Node node){
+        connections.add(node);
+    }
+
+    public void setSymbol(char symbol){
+        this.symbol = symbol;
+    }
+
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+    public void addParent(Node parent) {
+        this.parent = parent;
+    }
+
+    //get methods
+    public int getCost(){
+        return cost;
+    }
+
+    public char getSymbol(){
+        return symbol;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public ArrayList<Node> getConnections(){
+        return connections;
+    }
+
+    public Node getChild(int i) {
+        return connections.get(i);
+    }
+
+    public boolean getVisited() {return visited;}
+
+    public CopyNode getPointer()
+    {
+        return pointerNode;
+    }
+    public void setPointer(CopyNode inNode)
+    {
+        pointerNode = inNode;
+    }
+    public void setUnvisited()
+    {
+        visited = false;
+    }
+
+}
